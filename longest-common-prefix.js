6882f22c1f5db88c805b7c020824c95095c1018d
ayiyiyiyi <345930286@qq.com>
2020-05-21 21:06:09 +0800

    Roman to integer

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -3,20 +3,19 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-    let string = ''
+    const string = '';
     if (strs.length === 0) {
-        return string
+        return string;
     }
-    let first = strs.shift();
+    const first = strs.shift();
     for (let i = 0; i < first.length; i++) {
-        let firstStr = first.substr(0, i + 1)
-        let num = strs.length;
-        let common = strs.every(item => {
-            return item.substr(0, i + 1) === firstStr
+        const firstStr = first.substr(0, i + 1);
+        const common = strs.every(item => {
+            return item.substr(0, i + 1) === firstStr;
         })
         if (common) {
-            string = firstStr
+            string = firstStr;
         }
     }
-    return string
+    return string;
 };
--- roman-to-integer.js
+++ roman-to-integer.js
@@ -19,3 +19,11 @@ var romanToInt = function (s) {
     }
     return number;
 };
+
+function romanToIntTwo (s) {
+    const map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
+    const str = Array.from(s);
+    for (let i = 0; i < str.length; i++) {
+        
+    }
+}
