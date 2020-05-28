b3252b014423c15b5dfd98d4ff6d2643557c6df4
ayiyiyiyi <345930286@qq.com>
2020-05-28 14:13:39 +0800

    pairs-of-songs-with-total-durations-divisible-by-60 两数相加能被60整除的对数

--- array/pairs-of-songs-with-total-durations-divisible-by-60.js
+++ array/pairs-of-songs-with-total-durations-divisible-by-60.js
@@ -18,4 +18,23 @@ var numPairsDivisibleBy60 = function(time) {
     return res;
 };
 
-numPairsDivisibleBy60([30,20,150,100,40])
+// numPairsDivisibleBy60([30,20,150,100,40])
+
+function numPairsDivisibleBy60_two(time) {
+    let array = Array.from({length: 60},() => 0)
+    time.forEach(i => {
+        const n = i % 60;
+        array[n] = array[n] + 1;
+    });
+    let res = 0;
+    // 在30 和 0 是,需要两两排列组合 也就是 C(n, 2) = n * (n - 1) /2
+    res += array[0] * (array[0]  - 1) / 2;
+    res += array[30] * (array[30]  - 1) / 2;
+
+    for (let i = 1; i < 30; i++) {
+        res += array[i] * array[60 - i];
+    }
+    console.log(res)
+}
+numPairsDivisibleBy60_two([30,20,150,150,100,40])
+numPairsDivisibleBy60_two([60,60,60])
